49b5261f88e8f6b73a50c356e6eab50eddd34c40
neekey <ni184775761@gmail.com>
2016-05-31 00:16:25 +1000

    add nodegit test

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,25 @@
+var fs = require( 'fs' );
+var Git = require("nodegit");
+
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+});
+
+Parse.Cloud.define('nodegit-test', function( req, res) {
+
+    console.log( 'receive nodegit' );
+    var random = ( Math.random()*100 ).toFixed(0);
+  // Clone a given repository into the `./tmp` folder.
+  Git.Clone("https://github.com/neekey/ps.git", "./public/tmp" + random )
+      // Look up this known commit.
+      .then(function(repo) {
+        // Use a known commit sha from this repository.
+          console.log( 'success' );
+        return res.success( 'success::' + random );
+      })
+      .catch(function(err) {
+          console.log( 'fail', err.message );
+          res.error(err.message)
+      });
 });
